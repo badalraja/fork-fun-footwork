8d6f45f329a4c61274e1103055842ae7d19988eb
badalraja <badal.raja@kmart.com.au>
2023-08-26 05:48:40 +1000

    first commit

--- fff_test.ts
+++ fff_test.ts
@@ -4,5 +4,5 @@ Scenario("test something", ({ I }) => {
   I.amOnPage("/watch?v=i23iz8ZL9VI&list=PLrqYce371ayDCVx9i-SQXNgnebxP1uIk3");
   I.waitForElement("#movie_player");
   I.click(".ytp-play-button");
-  I.wait(200);
+  I.wait(10800);
 });
